395b18782cf08decca8ddd0957500ccf7e054bdd
wpannell <wil.pannell@gmail.com>
2014-02-17 01:15:47 -0800

    passing test

---
+++ app/js/Chapter5.js
@@ -0,0 +1,14 @@
+var chapter5 = {};
+
+chapter5.Airity = (function() {
+  'use strict';
+  function Airity() {}
+
+  Airity.prototype.function0 = function() {};
+  Airity.prototype.function1 = function(p1) {};
+  Airity.prototype.function10 = function(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10) {};
+
+  return Airity;
+})();
+
+
