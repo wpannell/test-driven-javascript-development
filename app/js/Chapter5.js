9dc2c9ee29a750308f3ac00111385d1f07b0e2f0
wpannell <wil.pannell@gmail.com>
2014-02-19 16:58:36 -0800

    simplified production code; purged .idea webstrom config

--- app/js/Chapter5.js
+++ app/js/Chapter5.js
@@ -1,46 +1,25 @@
-var chapter5 = {};
+function Airity() {}
 
-chapter5.Airity = (function() {
+Airity.prototype.function0 = function() {};
+Airity.prototype.function1 = function(p1) {};
+Airity.prototype.function10 =
+    function(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10) {};
+
+function addToArray() {
+  var targetArr = arguments[0];
+  var add = Array.prototype.slice.call(arguments, 1);
+  return targetArr.concat(add);
+}
+
+function changeFormalParameters(a, b) {
+  b = 42;
+  arguments[0] = arguments[1];
+  return a;
+}
+
+function changeFormalParametersE5Strict(a, b) {
   'use strict';
-  function Airity() {}
-
-  Airity.prototype.function0 = function() {};
-  Airity.prototype.function1 = function(p1) {};
-  Airity.prototype.function10 = function(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10) {};
-
-  return Airity;
-})();
-
-
-chapter5.addToArray = (function() {
-  'use strict';
-  function addToArray() {
-    var targetArr = arguments[0];
-    var add = Array.prototype.slice.call(arguments, 1);
-    return targetArr.concat(add);
-  }
-
-  return addToArray;
-})();
-
-chapter5.changeFormalParameters = (function() {
-  function changeFormalParameters(a, b) {
-    b = 42;
-    arguments[0] = arguments[1];
-    return a;
-  }
-
-  return changeFormalParameters;
-})();
-
-chapter5.changeFormalParametersE5Strict = (function() {
-  'use strict';
-  function changeFormalParameters(a, b) {
-    b = 42;
-    arguments[0] = arguments[1];
-    return a;
-  }
-
-  return changeFormalParameters;
-})();
-
+  b = 42;
+  arguments[0] = arguments[1];
+  return a;
+}
--- spec/Chapter5.coffee
+++ spec/Chapter5.coffee
@@ -1,21 +1,22 @@
 describe "chapter 5", ->
   describe "section 5.1", ->
     describe "a function's airity is the number of formal parameters", ->
-      Given -> @airity = new chapter5.Airity()
+      Given -> @airity = new Airity()
       Then -> 0 == @airity.function0.length
       Then -> 1 == @airity.function1.length
       Then -> 10 == @airity.function10.length
 
   describe "section 5.2", ->
     describe "the arguments object is like an array", ->
-      describe "chapter5.addToArray([1,'2',3,'four'], 'four', 3,'2',1)", ->
-        Then -> expect([1,'2',3,'four','four',3,'2',1]).toEqual(
-          chapter5.addToArray([1,'2',3,'four'], 'four', 3,'2',1))
+      Then -> expect([1,'2',3,'four','four',3,'2',1]).toEqual(
+        addToArray([1,'2',3,'four'], 'four', 3,'2',1))
 
     describe "when a property of the arguments object is changed, then", ->
       describe "so is the corresponding formal parameter, and conversely", ->
-        Then -> 42 == chapter5.changeFormalParameters(0,0)
+        Then -> 42 == changeFormalParameters(0,0)
 
       describe "unless in strict mode --  a recommended practice", ->
-        Then -> 42 != chapter5.changeFormalParametersE5Strict(0,0)
+        Then -> 42 != changeFormalParametersE5Strict(0,0)
+
+
 
